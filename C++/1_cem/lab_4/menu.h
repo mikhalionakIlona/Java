29906f0e50634cd35daf76d458afd4108a53b333
ilona <mihalenokilona@gmail.com>
2026-02-12 13:11:33 +0300

    Delete C++ directory

--- C++/1_cem/lab_4/menu.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "List.cpp"
-
-template<typename T>
-void work_list();
-
-void menu_selection();
-void menu_airplane(Airplane airplane);
--- C++/1_cem/lab_5/menu.h
+++
@@ -1,7 +0,0 @@
-#include "List.cpp"
-
-template<typename T>
-void work_list();
-
-void menu_selection();
-void menu_airplane(Airplane airplane);
