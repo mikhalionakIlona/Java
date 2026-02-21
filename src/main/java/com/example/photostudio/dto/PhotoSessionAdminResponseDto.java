c591f754e8bec496ad2dded921175edb14dd9eb8
ilona <mihalenokilona@gmail.com>
2026-02-21 10:24:30 +0300

    Add files via upload

---
+++ src/main/java/com/example/photostudio/dto/PhotoSessionAdminResponseDto.java
@@ -0,0 +1,155 @@
+package com.example.photostudio.dto;
+
+import java.time.LocalDateTime;
+
+public class PhotoSessionAdminResponseDto {
+    private Long id;
+    private String clientLastName;
+    private String clientName;
+    private String clientPhone;
+    private LocalDateTime photoSessionDate;
+    private double price;
+    private String photographer;
+    private String status;
+
+    public PhotoSessionAdminResponseDto() {
+    }
+
+    private PhotoSessionAdminResponseDto(Builder builder) {
+        this.id = builder.id;
+        this.clientLastName = builder.clientLastName;
+        this.clientName = builder.clientName;
+        this.clientPhone = builder.clientPhone;
+        this.photoSessionDate = builder.photoSessionDate;
+        this.price = builder.price;
+        this.photographer = builder.photographer;
+        this.status = builder.status;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String clientLastName;
+        private String clientName;
+        private String clientPhone;
+        private LocalDateTime photoSessionDate;
+        private double price;
+        private String photographer;
+        private String status;
+
+        private Builder() {
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder clientLastName(String clientLastName) {
+            this.clientLastName = clientLastName;
+            return this;
+        }
+
+        public Builder clientName(String clientName) {
+            this.clientName = clientName;
+            return this;
+        }
+
+        public Builder clientPhone(String clientPhone) {
+            this.clientPhone = clientPhone;
+            return this;
+        }
+
+        public Builder photoSessionDate(LocalDateTime photoSessionDate) {
+            this.photoSessionDate = photoSessionDate;
+            return this;
+        }
+
+        public Builder price(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder photographer(String photographer) {
+            this.photographer = photographer;
+            return this;
+        }
+
+        public Builder status(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public PhotoSessionAdminResponseDto build() {
+            return new PhotoSessionAdminResponseDto(this);
+        }
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public String getClientLastName() {
+        return clientLastName;
+    }
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public LocalDateTime getPhotoSessionDate() {
+        return photoSessionDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getPhotographer() {
+        return photographer;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public void setClientLastName(String clientLastName) {
+        this.clientLastName = clientLastName;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
+    }
+
+    public void setPhotoSessionDate(LocalDateTime photoSessionDate) {
+        this.photoSessionDate = photoSessionDate;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPhotographer(String photographer) {
+        this.photographer = photographer;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
